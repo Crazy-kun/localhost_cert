29d1ec02e62248e83a5ff32b30ad5a38e78b1cb8
Crazy-kun <evgen530@gmail.com>
2022-06-10 14:42:44 +0300

    Update .sh

--- create_cert.sh
+++ create_cert.sh
@@ -17,14 +17,10 @@ COMMON_NAME=${2:-$1}
 SUBJECT="/C=CA/ST=None/L=NB/O=None/CN=$COMMON_NAME"
 NUM_OF_DAYS=999
 
-openssl req -new -newkey rsa:2048 -sha256 -nodes $KEY_OPT device.key -subj "$SUBJECT" -out device.csr
+openssl req -new -newkey rsa:2048 -sha256 -nodes $KEY_OPT $DOMAIN.key -subj "$SUBJECT" -out $DOMAIN.csr
 
 cat v3.ext | sed s/%%DOMAIN%%/$COMMON_NAME/g > /tmp/__v3.ext
 
-openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days $NUM_OF_DAYS -sha256 -extfile /tmp/__v3.ext
+openssl x509 -req -in $DOMAIN.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out $DOMAIN.crt -days $NUM_OF_DAYS -sha256 -extfile /tmp/__v3.ext
 
-mv device.csr $DOMAIN.csr
-cp device.crt $DOMAIN.crt
-
-# remove temp file
-rm -f device.crt;
+rm -f $DOMAIN.csr
